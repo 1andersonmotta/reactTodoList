797bc228fb38529d21f1f69bb30f81d9e81ac6be
1andersonmotta <devandersonmotta@gmail.com>
2023-09-21 18:08:10 -0300

    feature adjust and test

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 
-const Todo = ({ todo }) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
     return (
-        <div className="todo" key={todo.id}>
+        <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
             <div className="content">
                 <p>{todo.text}</p>
                 <p className='category'>({todo.category})</p>
             </div>
             <div>
-                <button>Completar</button>
-                <button>x</button>
+                <button className="complete" onClick={() => completeTodo(todo.id)}>Completar</button>
+                <button className="remove" onClick={() => removeTodo(todo.id)}>x</button>
             </div>
         </div>
     )
